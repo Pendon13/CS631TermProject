7e4d5523e1bd7550547f3e44aa150a5bfd14958b
Pendon13 <pendon.jon@gmail.com>
2023-04-17 13:32:33 -0400

    added some data lines to the database

--- initialize.php
+++ initialize.php
@@ -11,18 +11,21 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+echo "<br>";
 $sql = "DROP DATABASE woodysDB";
 if ($conn->query($sql) === TRUE) {
   echo "Database dropped successfully";
 } else {
   echo "Error creating woodysDB database: " . $conn->error;
 }
+echo "<br>";
 $sql = "CREATE DATABASE woodysDB";
 if ($conn->query($sql) === TRUE) {
   echo "Database created successfully";
 } else {
   echo "Error creating woodysDB database: " . $conn->error;
 }
+echo "<br>";
 
 $dbname = "woodysdb";
 
@@ -35,6 +38,7 @@ if (mysqli_multi_query($conn, $query)){
 else {
   echo "Fail: " . $conn->error;
 }
+echo "<br>";
 //load files
 $conn = new mysqli($servername, $username, $password, $dbname);
 $query = file_get_contents("loadData.sql");
@@ -44,4 +48,5 @@ if (mysqli_multi_query($conn, $query)){
 else {
   echo "Fail: " . $conn->error;
 }
+echo "<br>";
 ?>
--- loadData.sql
+++ loadData.sql
@@ -1,8 +1,20 @@
 /* Customer */
+INSERT INTO `customer` (`id`, `fname`, `minit`, `lname`, `haddress`, `phone`, `creditcard`, `email`)
+    VALUES  (NULL, 'John', 'A', 'Smith', '816 Washington Ave, Hoboken, NJ', '2015551822', '1234123412341234', 'johnsmith@example.com'),
+            (NULL, 'Jane', NULL, 'Doe', '51 Franklin Rd, East Brunswick, NJ', '2015558162', '2345234523452345', 'janedow@example.com'),
+            (NULL, 'Mark', 'S', 'Hale', '321 Moat Dr, Paramus, NJ', '2015558261', '3456345634563456', 'markhale@example.com'),
+            (NULL, 'Sam', 'B', 'Roberts', '61 Englewood St, Trenton, NJ', '2015557162', '4567456745674567', 'samroberts@example.com'),
+            (NULL, 'Jay', NULL, 'Dale', '12 Broad St, Newark, NJ', '2015557281', '5678567856785678', 'jaydale@example.com');
+/* Vehicle */
 
 /* Part */
 
 /* Employees */
-INSERT INTO Employee VALUES()
+
 /* Locations */
-INSERT INTO `businesslocation` (`loc_address`) VALUES ('Newark, NJ'), ('Trenton, NJ'), ();
+INSERT INTO `businesslocation` (`loc_address`) 
+    VALUES  ('Newark, NJ'), 
+            ('Trenton, NJ'), 
+            ('Hoboken, NJ'), 
+            ('Paramus, NJ'), 
+            ('East Brunswick, NJ');
